bd0693a3b3a9ec5a1aa959811564969d4b18bc49
Okyost <Gr1nya24@yandex.ru>
2020-03-02 10:55:17 +0300

    Initial commit

---
+++ src/Primes.java
@@ -0,0 +1,24 @@
+public class Primes {
+    public static void main(String[] args){
+        for(int i = 2; i < 101; i++){
+            if (isPrime(i)){
+                System.out.print(i);
+                System.out.print(' ');
+            }
+    }
+
+    }
+    public static boolean isPrime(int num) {
+        int del = 0;
+        for (int iteration = 1; iteration < num; iteration++) {
+            if (num % iteration == 0) {
+                del += 1;
+            }
+        }
+        if (del <= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
